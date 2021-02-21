660ccfae8297982911e429bec11da59dfccf3670
Karol Ciołczyk <dyweko@gmail.com>
2021-02-21 13:44:04 +0100

    few chages in project2 in main.css

--- project3/main.js
+++ project3/main.js
@@ -100,7 +100,7 @@ background.addEventListener('click', () =>{
     background.style.display = 'none';
 });
 
-/*******************************************/
+/********************BUTTON ALL FILES %***********************/
 
 const gbFiles = document.querySelectorAll('.files-weight h4');
 const storageUsage = document.querySelector('.storage-usage h3');
@@ -112,21 +112,36 @@ const buttonPercent = document.querySelector('.button-percent');
 
 buttonPercent.addEventListener('click', () =>{
 
+    let score = 0;
+
     inCircleNum.textContent = `${''}`
-    gbFiles.forEach( e =>{
+    gbFiles.forEach( (e, index) =>{
         let number = e.textContent.match(/(\d+)/)[0];
         let valNum = parseFloat(number);
         inCircleNum.textContent += `${valNum}+`;
+        score += valNum
 
+        
     });
+
+    console.log(score);
     const inCircleNumSum = document.querySelector('.circle h3');
     let sumAll = inCircleNumSum.textContent + '0'
     let valNum = eval(sumAll);
-    inCircleNum.textContent = `${Math.round(valNum/1285*100)}%`
+
+    let output = 0;
+    const timer = setInterval(()=>{
+        inCircleNum.textContent = `${Math.round(output/1285*100)}%`
+        if(output === Math.round(valNum)){
+            clearInterval(timer)
+        } else{
+            output++;
+        }
+    }, 1);
     
 });
 
-
+/********************BUTTON ALL FILES % - soluction 2 ***********************/
 
 
 gbFiles.forEach( (e) =>{
@@ -149,6 +164,7 @@ gbFiles.forEach((e) =>{
 
         inCircleNum.textContent = `${Math.round(x/1285*100)}%`
 
+        
     });
     // storagaUsage.textContent = `${}`
 });
@@ -189,3 +205,144 @@ buttonDDL.addEventListener('mouseout', (e) =>{
 //     console.log(pointNum);
 // });
 
+console.log(buttonDDL.style);
+
+const mcmP = document.querySelector('.mcm>div p');
+const mcmDiv = document.querySelectorAll('.middle-color-menu div');
+   
+console.log(mcmP.textContent.includes('travel'));
+console.log(mcmDiv);
+
+console.log(mcmP.children);
+console.log(mcmP.parentElement);
+console.log(mcmP.parentElement.parentElement);
+console.log(mcmP.parentElement.nextElementSibling);
+console.log(mcmP.parentElement.nextElementSibling.previousElementSibling);
+
+mcmDiv.forEach(item =>{
+    item.addEventListener('click', e =>{
+        if(e.target.tagName === 'DIV'){
+            e.target.remove();
+        }
+        
+    });
+});
+
+
+const myForm = document.querySelector('.form');
+const usernamePattern = /^[a-zA-z]{6,8}$/
+const changeBackgr = document.querySelector('.form [type="text"]');
+// const username = document.querySelector('#user-name')sa
+
+myForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    // console.log(username.value);
+    console.log(myForm.username.value);
+
+    const username = myForm.username.value;
+    
+
+    if(usernamePattern.test(username)){
+        console.log('it is veeeeery good')
+    } else{
+        console.log('it is veeeerrrrry baaaaaaad')
+    }
+});
+
+myForm.addEventListener('keyup', e =>{
+    console.log(e.target.value, myForm.username.value);
+
+    if(usernamePattern.test(myForm.username.value)){
+        console.log('its gooooooooood')
+        // changeBackgr.classList.remove('baad');
+        // changeBackgr.classList.add('good');
+        changeBackgr.setAttribute('class', 'good');
+        
+    } else {
+        console.log('its baaaaaaaaaadddddddd')
+        // changeBackgr.classList.remove('good');
+        // changeBackgr.classList.add('baad');
+        changeBackgr.setAttribute('class', 'baad');
+    }
+});
+
+/********************FORM-RADIO***********************/
+
+const correctAnswers = ['A', 'A', 'A', 'A']
+const formRadio = document.querySelector('.form-radio')
+
+formRadio.addEventListener('submit', e=>{
+    e.preventDefault();
+
+    let score = 0;
+    let userAnswer = [formRadio.q1.value, formRadio.q2.value, formRadio.q3.value, formRadio.q4.value]
+
+    userAnswer.forEach((answer, index) =>{
+        console.log(answer, index);
+        if(answer === correctAnswers[index]){
+            score += 33;
+        }
+    });
+
+    let sc = 0;
+    let effect = setInterval(()=>{
+        document.querySelector('.score-value span').style.fontSize = '40px'
+        document.querySelector('.score-value span').textContent = `${sc}`;
+        if(sc === score){
+            clearInterval(effect);
+        } else{
+            sc++
+        }
+    },10);
+});
+
+
+/****************************** ARRAY METHODS ****************************/
+
+
+
+// FILTER METHOD
+const scores = [40, 50, 10, 10, 45, 25, 30, 88, 24, 5, 54, 8, 12];
+
+const userLevel = [
+    {username: 'stefan', vip: true},
+    {username: 'wacek', vip: true},
+    {username: 'mustafa', vip: false},
+    {username: 'mietek', vip: true},
+    {username: 'bożek', vip: false},
+    {username: 'stasiek', vip: false},
+    {username: 'kajtek', vip: false},
+]
+
+let scoreFiltered = userLevel.filter((user)=>{
+    return user.vip
+});
+
+console.log(scoreFiltered);
+
+// MAP METHOD
+
+const vegePrice = [
+    {name: 'carrot', price: 30},
+    {name: 'cucumber', price: 20},
+    {name: 'potato', price: 14},
+    {name: 'tomato', price: 8},
+    {name: 'onion', price: 4},
+    {name: 'banana', price: 19},
+    {name: 'grape', price: 28},
+    {name: 'blackberry', price: 24},
+    {name: 'pistacio', price: 41},
+    {name: 'berry', price: 35},
+    {name: 'apple', price: 16},
+];
+
+let saleVege = vegePrice.map((product)=>{
+    if(product.price > 20) {
+        return { name: product.name, price: product.price / 2}
+    } else {
+       return product;
+    }
+});
+
+console.log(saleVege);
+console.log(vegePrice);
