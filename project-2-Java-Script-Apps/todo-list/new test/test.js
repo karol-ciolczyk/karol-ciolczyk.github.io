e3aa40d1edc2c2efca41174a16ba6b08b6b9a2dc
Karol Ciołczyk <dyweko@gmail.com>
2021-04-15 00:50:02 +0200

    test weatherApp function added, function creats weather-condition object

--- project-2-Java-Script-Apps/todo-list/new test/test.js
+++ project-2-Java-Script-Apps/todo-list/new test/test.js
@@ -145,4 +145,15 @@ cleanButton.addEventListener('click', ()=>{
     isEmpty();
 })
 
+const whatCity = document.querySelector('.whatCity');
+const inp = document.querySelector('.whatCity input')
+whatCity.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    console.log(inp.value);
+    let city = inp.value;
+
+
+getLocation(city);
+})
+
 // getLocation(`Alwernia`);
