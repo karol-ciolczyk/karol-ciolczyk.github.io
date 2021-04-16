201ec665d1b0fd4b68ec7db506a22ff23677ae1d
Karol Ciołczyk <dyweko@gmail.com>
2021-04-16 10:50:09 +0200

    weatherApp almost finished

--- project-2-Java-Script-Apps/todo-list/new test/modules/weatherApp/getLocation.js
+++ project-2-Java-Script-Apps/todo-list/new test/modules/weatherApp/getLocation.js
@@ -4,23 +4,46 @@ export const getLocation = (city)=>{
     fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=1c7TBu4MybMcEKxCr30xDolto2UWRYDl&q=${city}`)
     .then(response => response.json())
     .then(data => {
-        const newObject = {};
+        const weatherObject = {};
 
         console.log(data[0]);
         let key = data[0].Key;
 
-        newObject.city = `${data[0].EnglishName}`;
-        newObject.type = `${data[0].Type}`;
+        weatherObject.city = `${data[0].EnglishName}`;
+        weatherObject.type = `${data[0].Type}`;
 
         getWeatherConditions(key).then(data => {
             console.log(data[0]);
-            newObject.condition = data[0].WeatherText;
-            newObject.icon = data[0].WeatherIcon;
-            newObject.isDay = data[0].IsDayTime;
-            newObject.temperature = data[0].Temperature.Metric.Value;
+            weatherObject.condition = data[0].WeatherText;
+            weatherObject.icon = data[0].WeatherIcon;
+            weatherObject.isDay = data[0].IsDayTime;
+            weatherObject.temperature = data[0].Temperature.Metric.Value;
 
 
-            console.log(newObject);
+            console.log(weatherObject);
+
+            // values from weatherObject:
+            const location = weatherObject.city;
+            const iconNumber = weatherObject.icon;
+            const isDay = weatherObject.isDay;
+            const temp = weatherObject.temperature;
+            const cond = weatherObject.condition;
+
+            // change weather/location values in DOM
+            const city = document.querySelector('.city');
+            const temperature = document.querySelector('.temperature span');
+            const condition = document.querySelector('.condition');
+            const icon = document.querySelector('.icon img');
+            const appContainer = document.querySelector('.weatherApp-container')
+
+            city.textContent = `${location}`;
+            temperature.textContent = `${temp}`;
+            condition.textContent = `${cond}`;
+            icon.setAttribute(`src`, `./img/icons/${iconNumber}.svg`);
+            appContainer.style.background = `url('./img/${isDay}.jpg')`;
+            appContainer.style.backgroundSize = `cover`;
+
+
         })
 
     })
