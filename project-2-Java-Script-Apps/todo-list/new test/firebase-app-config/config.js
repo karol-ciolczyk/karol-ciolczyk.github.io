72ec785a9b6f6cc096ac40ca300f8e3e84641221
Karol Ciołczyk <dyweko@gmail.com>
2021-04-19 12:38:40 +0200

    firestore config added, firestor app + librarie added

---
+++ project-2-Java-Script-Apps/todo-list/new test/firebase-app-config/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAH-C0Y2zOYfNgql1YCTQHBR--4n7VPMjo",
+    authDomain: "todo-list-bae9a.firebaseapp.com",
+    databaseURL: "https://todo-list-bae9a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todo-list-bae9a",
+    storageBucket: "todo-list-bae9a.appspot.com",
+    messagingSenderId: "318127646764",
+    appId: "1:318127646764:web:4c10c8ae18088a938fc989"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
